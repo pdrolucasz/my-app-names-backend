93d016fe7d80d57793aa614c4c3d1d4f653f7872
pdrolucasz <pedro.lucas4431@gmail.com>
2021-04-27 11:00:01 -0300

    change services

--- src/services/ListNameService.ts
+++ src/services/ListNameService.ts
@@ -6,7 +6,7 @@ class ListNameService {
   public async execute(id: string): Promise<Name> {
     const nameReporitory = getMongoRepository(Name)
 
-    const listName = await nameReporitory.findOne(id)
+    const listName = await nameReporitory.findOneOrFail(id)
 
     if(!listName) {
       throw new Error('Name not found')
